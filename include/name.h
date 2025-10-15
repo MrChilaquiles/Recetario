93ee288418da9549e428048aeb73a94892a4c2af
MrChilaquiles <RGGA2000@gmail.com>
2025-10-15 00:07:05 -0600

    Add Name class definition with basic member functions and operators

---
+++ include/name.h
@@ -0,0 +1,41 @@
+#ifndef __NAME_HPP__
+#define __NAME_HPP__
+
+#include<string>
+#include<iostream>
+
+class Name
+{
+private:
+    std::string last;
+    std::string first;
+
+public:
+    Name();
+    Name(const Name&);
+
+    std::string getLast() const;
+    std::string getFirst() const;
+
+    std::string toString() const;
+
+    void setLast(const std::string&);
+    void setFirst(const std::string&);
+
+    Name& operator = (const Name&);
+
+    bool operator == (const Name&) const;
+    bool operator != (const Name&) const;
+    bool operator < (const Name&) const;
+    bool operator <= (const Name&) const;
+    bool operator > (const Name&) const;
+    bool operator >= (const Name&) const;
+
+    int compareTo(const Name&) const;
+    static int compare(const Name&, const Name&);
+
+    friend std::ostream& operator << (std::ostream&, const Name&);
+    friend std::istream& operator >> (std::istream&, Name&);
+
+};
+#endif // __NAME_HPP__

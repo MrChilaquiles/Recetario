c09443dfe702889a57114b3835659534f7294929
MrChilaquiles <RGGA2000@gmail.com>
2025-10-15 00:07:22 -0600

    Add Ingredient class definition with member functions and operators

---
+++ include/ingredient.h
@@ -0,0 +1,47 @@
+#ifndef __INGREDIENT_H__
+#define __INGREDIENT_H__
+
+#include <iostream>
+#include <string>
+
+class Ingredient {
+private:
+    std::string name;
+    float amount;
+    std::string unit;
+
+public:
+    Ingredient();
+    Ingredient(const Ingredient&);
+
+    std::string getName() const;
+    float amount() const;
+    std::string getUnit() const;
+
+    void setName(const std::string&);
+    void setAmount(const float&);
+    void setUnit(const std::string&);
+
+    std::string toString() const;
+
+    Ingredient& operator = (const Ingredient&);
+
+    bool operator == (const Ingredient&) const;
+    bool operator != (const Ingredient&) const;
+    bool operator < (const Ingredient&) const;
+    bool operator <= (const Ingredient&) const;
+    bool operator > (const Ingredient&) const;
+    bool operator >= (const Ingredient&) const;
+
+    int compareTo(const Ingredient&) const;
+    static int compare(const Ingredient&, const Ingredient&);
+
+    static int compareByName(const Ingredient&, const Ingredient&);
+    static int compareByAmount(const Ingredient&, const Ingredient&);
+    static int compareByUnit(const Ingredient&, const Ingredient&);
+
+    friend std::ostream& operator << (std::ostream&, Ingredient&);
+    friend std::istream& operator >> (std::istream&, Ingredient&);
+};
+
+#endif // __INGREDIENT_H__
